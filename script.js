e34a87a682c0ade20c913718d23f0e07e5980962
Leticia Santos Moura <letistos@gmail.com>
2023-01-17 11:51:06 -0300

    Commit 4 - shuffle

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 // -- saudação -- //
-prompt('com quantas cartas quer jogar?');
+teste();
+
+function teste(){
 
 
+let pergunta = false;
+
+while(pergunta === false){
+
+const qtde = prompt('com quantas cartas quer jogar?');
+    
+if(qtde < 4 || qtde > 14 || qtde % 2 !== 0  ){
+        alert('quantidade de cartas inválida');
+        pergunta = false;
+    }
+       else{
+        pergunta = true;
+        return pergunta;
+       }
+}
+}
 // -- fim saudação -- //
 
 // -- dispor cartas no main -- //
 
+//const cartasTabuleiro = document.querySelectorAll('.card-box');
+//cartasJogo = [];
 
+//function tabuleiro(qtde){   
+//
+  //  const displayUsuario = Array.prototype.slice.call(cartasTabuleiro);
+   // for (let i = 0; i < qtde; i++) {
+
+     //   cartasJogo.push(i);
+  // }
+//}
+// -- fim de dispor cartas no main -- //
+
+//  virar cartas //
 
 const deck = document.querySelectorAll('.card-box');
 
@@ -16,11 +47,11 @@ let cartaUm;
 
 let cartaDois;
 
-//  virar cartas //
 
 function virarCarta(){
 
     this.classList.add('flip');
+    contadorCliques();
 
     if(estaVirada == false){
     estaVirada = true;
@@ -36,22 +67,27 @@ function virarCarta(){
 // -- fim virar cartas -- //
 
 // --encontrar pares --//
-
+let finalJogo = 0;
 function encontrarPares(){
    let par =  cartaUm.dataset.card == cartaDois.dataset.card;
+   if (par == true){
+    finalJogo++;
+   }
    if(par == false){
 
    desvirarCarta();
    }
+   
+
 }
 // --fim encontrar pares --//
 
 // --manter pares abertos--//
 
+
 function manterAberto(){
 
     cartaUm.addEventListener('click',flip);
-
 }
 // --fim de manter pares abertos -- //
 
@@ -66,21 +102,51 @@ function desvirarCarta(){
 
 }
 
+
 // -- fim de desvirar quando nao e par -- //
+
+// -- embaralhar cartas -- //
+
+
+(function embaralhar(){
+    deck.forEach(carta => {
+        let random = Math.floor (Math.random() * 14);
+        carta.style.order = random;
+    });
+})();
+
+// -- fim embaralhar cartas -- //
+
+
 deck.forEach(carta => carta.addEventListener('click',virarCarta));
 
 
 
-// -- embaralhar cartas -- //
-// -- fim embaralhar cartas -- //
-
 // -- contar cliques -- //
+function executarAmbas(){
+    virarCarta();
+    contadorCliques();
+
+}
+
+let clique = 0;
+function contadorCliques(){
+   clique++;
+}
+
 // -- fim contar cliques -- //
 
+// -- despedida -- //
+function despedida(){
 
+    if(finalJogo == 14){
 
-//let cartaUm
-//let cartaDois
+    alert(`Você ganhou em ${clique} jogadas!`);
+
+    }
+}
+// -- fim despedida -- //
+
 
 
 
